a23ee8a224bf746f84c48cdb8ceae70dca50fb23
Konstantin Smetana <konstantin.smetana@baslerweb.com>
2024-05-22 15:49:58 +0200

    show locales

--- Model/Translation/Source/Locales.php
+++ Model/Translation/Source/Locales.php
@@ -5,40 +5,67 @@ namespace Phpro\Translations\Model\Translation\Source;
 
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Locale\Deployed\Options;
 
 class Locales implements OptionSourceInterface
 {
     private const XML_PATH_LOCALE = 'general/locale/code';
 
     /**
-     * @var ResourceConnection
+     * Constuctor
+     *
+     * @param \Magento\Framework\App\ResourceConnection $resourceConnection
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\Locale\Deployed\Options $localeOptions
      */
-    private $resourceConnection;
-
-    public function __construct(ResourceConnection $resourceConnection)
-    {
-        $this->resourceConnection = $resourceConnection;
+    public function __construct(
+        private ResourceConnection $resourceConnection,
+        private StoreManagerInterface $storeManager,
+        private ScopeConfigInterface $scopeConfig,
+        private Options $localeOptions
+    ) {
     }
 
     /**
-     * @inheritDoc
+     * Options for select element
+     *
+     * @return array
      */
-    public function toOptionArray()
+    public function toOptionArray(): array
     {
-        $result = [];
-        $connection = $this->resourceConnection->getConnection();
-        $bind = [':config_path' => self::XML_PATH_LOCALE];
-        $select = $connection
-            ->select()
-            ->from($this->resourceConnection->getTableName('core_config_data'), 'value')
-            ->distinct(true)
-            ->where('path = :config_path');
-        $rowSet = $connection->fetchAll($select, $bind);
+        $locale = [];
+        $stores = $this->storeManager->getStores($withDefault = false);
+        foreach ($stores as $store) {
+            $storeLang = $this->scopeConfig->getValue(
+                'general/locale/code',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                $store->getStoreId()
+            );
 
-        foreach ($rowSet as $row) {
-            $result[] = ['value' => $row['value'], 'label' => $row['value']];
+            $locale[] = $storeLang;
         }
 
-        return $result;
+        $locale = array_unique($locale);
+        sort($locale);
+
+        return $this->filterLocales($locale);
+    }
+
+    /**
+     * Get locales
+     *
+     * @param array $availableLocales
+     * @return array
+     */
+    private function filterLocales(array $availableLocales = []): array
+    {
+        $locales = $this->localeOptions->getOptionLocales();
+
+        return array_filter($locales, function ($localeData) use ($availableLocales) {
+            return in_array($localeData['value'], $availableLocales);
+        });
     }
 }
